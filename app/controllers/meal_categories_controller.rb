02e590c976856dc1007d3cd0e854366a63da7953
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-06-17 03:53:25 -0300

    desenvolver CRUD do meals controller

--- app/controllers/meal_categories_controller.rb
+++ app/controllers/meal_categories_controller.rb
@@ -1,16 +1,4 @@
 class MealCategoriesController < ApplicationController
 
-    #indexar todas as instâncias da model
-    def index 
-        @meals = Meal.all
-        render json:@meals, status: 200
-    end
-
-    def show 
-        @meals = Meal.find(params[:id])
-        render json:@meals, status: 200
-    end
-
-    
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
   get "/meals", to: "meals#index"
   get "/meals/:id", to: "meals#show"
+  post "/meals", to: "meals#create"
+  put "/meals/:id", to: "meals#update"
+  delete "/meals/:id", to: "meals#destroy"
 
 end
